ca5399e90bba0d1e39c28ca5fd2eac46bbe125ee
AldieryGonzalez <aldieryrgon@gmail.com>
2023-01-30 14:15:19 -0600

    Step 15: From a Skeleton

---
+++ parallelHillClimber.py
@@ -0,0 +1,37 @@
+from solution import SOLUTION
+import constants as c
+import copy
+
+
+class PARALLEL_HILL_CLIMBER:
+    def __init__(self):
+        # self.parent = SOLUTION()
+        pass
+
+    def Evolve(self):
+        # self.parent.Evaluate("GUI")
+        # for currentGeneration in range(c.numberOfGenerations):
+        #     self.Evolve_For_One_Generation()
+        pass
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Select()
+        print(
+            f"\n\nParent: {self.parent.fitness}, Child: {self.child.fitness}\n")
+
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    def Select(self):
+        if (self.parent.fitness > self.child.fitness):
+            self.parent = self.child
+
+    def Show_Best(self):
+        # self.parent.Evaluate("GUI")
+        pass
